0c4385eeaf2f564d9551b2471be7f5972ea028b4
Christina Gonzalez <cmgamway@gmail.com>
2019-09-11 04:20:18 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -11,7 +11,11 @@ class String
   end
 
   def question?
-    "What's your name?".question("?")
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
